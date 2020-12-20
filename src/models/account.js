470005ffe099d469a91c4ab060d317dfe5f6e2be
eti246 <desrocherset@hotmail.com>
2020-12-20 10:42:30 -0500

    eee

--- src/models/account.js
+++ src/models/account.js
@@ -1,11 +1,5 @@
 import mongoose from 'mongoose';
 
-const elements = [{name :"A", quantity: 0 },{name :"B", quantity: 0 } ,{name :"E", quantity: 0 }
-        ,{name :"Ex", quantity: 0 },{name :"Fr", quantity: 0 },{name :"G", quantity: 0 }
-        ,{name :"I", quantity: 0 },{name :"Ja", quantity: 0 },{name :"K", quantity: 0 }
-        ,{name :"L", quantity: 0 },{name :"No", quantity: 0 },{name :"Q", quantity: 0 }
-        ,{name :"Sm", quantity: 0 },{name :"Ve", quantity: 0 },{name :"Wu", quantity: 0 }
-        ,{name :"Xu", quantity: 0 },{name :"Ye", quantity: 0 },{name :"Z", quantity: 0 }];
 
 const accountSchema = mongoose.Schema(
     {
